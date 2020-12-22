25024949313e7d7b1f304d27dd0f32e654d0417d
Marcelo Corrêa <marcelocorrea229@gmail.com>
2020-12-22 00:17:39 -0300

    first commit

---
+++ src/Uuid.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MarceloCorrea\Uuid;
+
+use Ramsey\Uuid\Uuid as RamseyUuid;
+
+trait Uuid
+{
+    public static function bootUuid()
+    {
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = $model->{$model->getKeyName()} ?: (string) RamseyUuid::uuid4();
+        });
+    }
+}
